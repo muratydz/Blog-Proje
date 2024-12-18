81d2fd155a01cb67e6ff1aa9fb97b9e5da0b36ff
Murat Yıldız <20muratyildiz00@gmail.com>
2024-12-18 22:38:13 +0300

    create signin, signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -31,4 +34,37 @@ export const signup = async (req, res, next) => {
         next(error);
     }
 
-} 
+}
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const valideUser = await User.findOne({ email });
+
+        if (!valideUser) {
+            return next(errorHandler(400, "User not found"))
+        }
+        const validePassword = bcryptjs.compareSync(password, valideUser.password);
+        if (!validePassword) {
+            return next(errorHandler(400, "Invalid password"));
+        }
+        const token = jwt.sign(
+            { id: valideUser._id, isAdmin: valideUser.isAdmin },
+            process.env.JWT_SECRET_TOKEN
+        );
+
+        const { password: pass, ...rest } = valideUser._doc;
+
+        res.status(200).cookie("access_token", token, {
+            httpOnly: true,
+        }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+}
