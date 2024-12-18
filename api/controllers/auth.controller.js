22db30ea2bdc2a72a71d5a75775157f3e9d5899b
Murat Yıldız <20muratyildiz00@gmail.com>
2024-12-18 21:37:43 +0300

    user and auth api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === "" ||
+        email === "" ||
+        password === ""
+    ) {
+        next(errorHandler(404, "All fields are required"));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    try {
+        await newUser.save();
+        res.status(200).json("Singup successful");
+    } catch (error) {
+        next(error);
+    }
+
+} 
---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+import User from "../models/user.model.js";
+
+export const test = (req, res) => {
+    res.json({message: "API is working!"});
+}
