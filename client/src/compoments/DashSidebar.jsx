acd84c0debaa0fd2b3aa0c2a033316a7078663cc
Murat Yıldız <20muratyildiz00@gmail.com>
2024-12-23 22:24:30 +0300

    admin panel uı

--- client/src/compoments/DashSidebar.jsx
+++ client/src/compoments/DashSidebar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
 
 const DashSidebar = () => {
-  return (
-    <div>
-      DashSidebar 
-    </div>
-  )
-}
+  const location = useLocation();
 
-export default DashSidebar
+  const isActive = (path) => {
+    return location.search.includes(path);
+  };
+
+  return (
+    <div className="dashSidebar">
+      <Link 
+        to={"/admin-panel?tab=all-post"} 
+        className={`link ${isActive('all-post') ? 'link-active' : ''}`}
+      >
+        Post Management
+      </Link>
+      <Link 
+        to={"/admin-panel?tab=user-management"} 
+        className={`link ${isActive('user-management') ? 'link-active' : ''}`}
+      >
+        User Management
+      </Link>
+    </div>
+  );
+};
+
+export default DashSidebar;
--- client/src/page/AdminPanel.jsx
+++ client/src/page/AdminPanel.jsx
@@ -1,35 +1,35 @@
-import "../css/admin-panel.css"
 import React, { useEffect, useState } from 'react';
-import DashSidebar from '../compoments/DashSidebar';
-import {useLocation} from "react-router-dom";
-import DashPost from '../compoments/DashPost';
-import DashUser from "../compoments/DashUser";
-
+import { useLocation } from "react-router-dom";
+import DashSidebar from '../compoments/DashSidebar.jsx';
+import DashPost from '../compoments/DashPost.jsx';
+import DashUser from '../compoments/DashUser.jsx';
+import "../css/admin-panel.css";
 
 const AdminPanel = () => {
   const location = useLocation();
   const [tab, setTab] = useState("");
-  console.log(tab);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFormUrl = urlParams.get("tab");
-    if(tabFormUrl){
+    if (tabFormUrl) {
       setTab(tabFormUrl);
     }
-  }, [location.search])
+  }, [location.search]);
 
   return (
-    <div className='dashboard'>
-      <div className='dashSidebar'>
+    <div className="dashboard">
+      <div>
       <DashSidebar/>
+        
       </div>
-      <div className='dashCompoment'>
-        {tab === "all-post" && <DashPost/> }
-        {tab === "user-management" && <DashUser/>}
+      
+      <div className="dashCompoment">
+        {tab === "all-post" && <DashPost/>}
+        {tab === "user-management" && <DashUser />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPanel
+export default AdminPanel;
