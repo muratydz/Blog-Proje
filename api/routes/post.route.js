c059e1a1d46ca6c1312a3ee76e979bbfc6a16c42
Murat Yıldız <20muratyildiz00@gmail.com>
2025-01-11 21:24:46 +0300

    Implement APIs for updating, deleting, and fetching posts

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create , getPosts } from "../controllers/post.controller.js";
+import { create , deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId", verifyToken, deletePost);
+router.put("/updatepost/:postId", verifyToken, updatePost);
 
 export default router;
 
